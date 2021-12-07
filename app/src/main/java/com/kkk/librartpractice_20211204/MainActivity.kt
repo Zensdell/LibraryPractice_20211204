4326941e46145816c22bd4d42dca1167e6e40f03
Kunote <rndudah754@gmail.com>
2021-12-07 12:56:48 +0900

    CALL액션 활용 (전화연결) -> 권한문제로 앱이 죽는다.

--- app/src/main/java/com/kkk/librartpractice_20211204/MainActivity.kt
+++ app/src/main/java/com/kkk/librartpractice_20211204/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.kkk.librartpractice_20211204
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,6 +16,20 @@ class MainActivity : AppCompatActivity() {
 
 
     fun setupEvents() {
+
+        btnCall.setOnClickListener {
+
+//            전화 연결 해도되는지? 권환 확인 -> OK일때만 전화연결.
+
+//            010-2211-1111 에 실제 전화 연결
+            val myUri = Uri.parse("tel:010-2211-1111")
+            val myIntent = Intent(Intent.ACTION_CALL,myUri)
+            startActivity(myIntent)
+
+        }
+
+
+
 
         imgProfile.setOnClickListener {
 
